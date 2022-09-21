bc73a7e077a1cf87689b6b5663d09874b7f90dbd
Pedro Brito <pedrobfh@gmail.com>
2022-09-21 12:09:36 +0100

    Expressoes Condicionais

--- Paradigmas.py
+++ Paradigmas.py
@@ -1,5 +1,11 @@
 #Hello World
 
 #Iniciacao dos operadores e regra de operadores no Python
-x = 5 + 10/5 * (2+2)
-print(x)
+a = 5 + 10/5 * (2+2)
+print(a)
+
+#Iniciacao das expressoes condicionais
+b = 5 if 3 > 5 else (7 if 8 > 9 else 99)
+print(b)
+
+
