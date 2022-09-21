a540c6f0eb0d8c9bf363dfbc9eca91f984a129a1
Pedro Brito <pedrobfh@gmail.com>
2022-09-21 12:18:27 +0100

    Outras operacoes com numeros

--- Paradigmas.py
+++ Paradigmas.py
@@ -8,8 +8,14 @@ print(a)
 b = 5 if 3 > 5 else (7 if 8 > 9 else 99)
 print(b)
 
-#Operacoes com numeros
+#Operacoes com numeros - Divisao, Quociente da Divisao e Resto da divisao
 c = 7 / 2
 d = 7 // 2
 e = 7 % 2
 print (c, d, e)
+
+#Outras operacoes com numeros
+f = round(3.6)
+g = int(3.6)
+h = float(3.6)
+print(f, g, h)
