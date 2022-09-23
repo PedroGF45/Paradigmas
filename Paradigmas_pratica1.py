8c62f669beb62afc5e13014333b8c05892e2d8db
Pedro Brito <pedrobfh@gmail.com>
2022-09-23 10:40:13 +0100

    input e eval

--- Paradigmas_pratica1.py
+++ Paradigmas_pratica1.py
@@ -27,29 +27,40 @@ print("f=", f, ftype)
 
 g = 4.25e-1 + 8e-2
 gtype = type(g)
-print("g", g, gtype)
+print("g=", g, gtype)
 
 h = 9 // 4 == 7 % 5
 htype = type(h)
-print("h", h, htype)
+print("h=", h, htype)
 
 i = 8 // 2 == 8 / 2.0
 itype = type(i)
-print("i", i, itype)
+print("i=", i, itype)
 
 j = 3 > 2.0 and 7 > 8.5
 jtype = type(j)
-print("j", j, jtype)
+print("j=", j, jtype)
 
 k = 3.0 != 2 or 7.5 > 8
 ktype = type(k)
-print("k", k, ktype)
+print("k=", k, ktype)
 
-l = not(17 % 2 == 0 and 3 / 0 > 5)
+l = not(17 % 2 == 0 and 3 / 0 > 5) # A avaliacao e sequencial, senao dava erro pq nao se pode dividir por 0. Se o primeiro e falso, vai dar logo falso.
 ltype = type(l)
-print("l", l, ltype)
+print("l=", l, ltype)
 
-# m = true and False #erro
+# m = true and false # erro porque isto nao sao os valores booleanos (ele age como nomes)
 # mtype = type(m)
 # print("m", m, mtype)
 
+m1 = True and False
+m1type = type(m1)
+print("m1", m1, m1type)
+
+n, o = 2, 3 #aqui n = 2 e o = 3
+n, o = o, n #aqui trocam-se os valores e n = 3 e o = 2
+print("n=", n, "\no=", o)
+print("1 + 1 =", n) # Nao ha contas, apenas copia o que esta dentro das aspas
+
+p = eval(input("Escreva..")) # o que fica guardado no p e uma cadeia de caracteres #ver de dentro para fora
+print(p)
