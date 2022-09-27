4cc0f6ae18162c44e0150c2f992abfecc90440a2
Pedro Brito <pedrobfh@gmail.com>
2022-09-27 14:50:44 +0100

    Deleted parenthesis

--- Paradigmas_pratica2.py
+++ Paradigmas_pratica2.py
@@ -17,7 +17,7 @@ print(f"O Marcelo obteve {nota_Marcelo} % na frequencia. \n O Gabriel obteve {no
 # Ex 4
 
 def soma_Inteiros():
-    n = (input("Escreva um numero inteiro")) # receive the input from the user
+    n = input("Escreva um numero inteiro") # receive the input from the user
     try:
         n = int(n) # see if the value is an integer. If not, go to line 25
         soma = ((n)*(n + 1))/2
@@ -27,3 +27,4 @@ def soma_Inteiros():
         soma_Inteiros() # se nao for integer corre a funcao novamente
 
 soma_Inteiros()
+
