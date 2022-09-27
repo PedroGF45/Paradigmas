735f89e417a0c25857e7d4b77a46e35a81d688ad
Pedro Brito <pedrobfh@gmail.com>
2022-09-27 14:51:28 +0100

    Minor changes to sintax

--- Paradigmas_pratica2.py
+++ Paradigmas_pratica2.py
@@ -17,13 +17,13 @@ print(f"O Marcelo obteve {nota_Marcelo} % na frequencia. \n O Gabriel obteve {no
 # Ex 4
 
 def soma_Inteiros():
-    n = input("Escreva um numero inteiro") # receive the input from the user
+    n = input("Escreva um numero inteiro: ") # receive the input from the user
     try:
         n = int(n) # see if the value is an integer. If not, go to line 25
         soma = ((n)*(n + 1))/2
         print(f"A soma de 1 a {n} e de {soma:.0f}")
     except ValueError: 
-        print(f"{n} nao e um inteiro.")
+        print(f"{n} nao e um inteiro!")
         soma_Inteiros() # se nao for integer corre a funcao novamente
 
 soma_Inteiros()
