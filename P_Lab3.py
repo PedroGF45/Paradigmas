4615b5154c842e277c4fffe31f784fd2bef53cc3
Pedro Brito <pedrobfh@gmail.com>
2022-10-04 14:40:42 +0100

    Added exercise 2 from 'ficha 2'

--- P_Lab3.py
+++ P_Lab3.py
@@ -12,3 +12,15 @@ print(a[2][0]) # third term of a (8, 9) and first term of it (8)
 print(9 in a) # 9 isn't on a but (8, 9) is, so false
 print((1, 2) not in a) # (1, 2) isn't on a so it's true
 # a[1] = 'xpto' # we cannot change tuples
+
+# Ex 2
+
+b = ((5, 2, 8, 3, 9, 1, 10))
+c = b[4:] # Create a tuple from the indice 4 onwards
+d = b[4:7] # Create a tuple from the indice 4 till indice 7-1=6
+e = (b[4],) + (b[5],) + (b[6],) # Create a tuple concatenating the elements with indices 4, 5 and 6
+f = b[-3:] # Create a tuple from the indice -3 onwards - better choice
+print(c)
+print(d)
+print(e)
+print(f)
