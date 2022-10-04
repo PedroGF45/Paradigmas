2e3059840e61e3de0e4e12fe1c794887b8d786ae
Pedro Brito <pedrobfh@gmail.com>
2022-10-04 15:12:18 +0100

    Added comments for what to expect

--- P_Lab3.py
+++ P_Lab3.py
@@ -20,31 +20,35 @@ c = b[4:] # Create a tuple from the indice 4 onwards
 d = b[4:7] # Create a tuple from the indice 4 till indice 7-1=6
 e = (b[4],) + (b[5],) + (b[6],) # Create a tuple concatenating the elements with indices 4, 5 and 6
 f = b[-3:] # Create a tuple from the indice -3 onwards - better choice
-print(c)
-print(d)
-print(e)
-print(f)
+print(c) # (9, 1, 10)
+print(d) # (9, 1, 10)
+print(e) # (9, 1, 10)
+print(f) # (9, 1, 10)
 
 # Ex 3
 
 g = ((5, 2, 8), (9, 1), (7, ), (4, 2, 9, 8))
 g_firstE_lastE = g[0][2]
-print(g_firstE_lastE)
+print(g_firstE_lastE) # 8
 g_fourthE_ThirdE = g[3][2]
-print(g_fourthE_ThirdE)
+print(g_fourthE_ThirdE) # 9
 h = (g[1],) + (g[-2],) * 3
-print(h)
+print(h) # ((9, 1), (7,), (7,), (7,))
 
 i = ()
 for x in range(0, len(g)): # for x getting the values from 0 to length in g tuple(4)
     i = i + (len(g[x]),) 
-    print(i)
+    print(i) 
+    # (3,)
+    # (3, 2)
+    # (3, 2, 1)
+    # (3, 2, 1, 4)
 
 j = tuple(len(y) for y in g) # I want the length of y for each y in tuple g
-print(j)
+print(j) # (3, 2, 1, 4)
 
 # Ex 4
 
 k = tuple(x for x in range(1, 26, 2)) # if range(1, 25, 2) it won't output 25
-print(k)
+print(k) # (1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25)
 
