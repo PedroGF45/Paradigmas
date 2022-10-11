6281d0bba76bf3ed515184a81fd874ad4f2cd555
Pedro Brito <pedrobfh@gmail.com>
2022-10-11 15:06:02 +0100

    Added exercise 1 from 'Ficha 3'

--- P_Ficha_3.py
+++ P_Ficha_3.py
@@ -2,3 +2,15 @@
 # Ficha 3
 
 # Ex 1
+
+alunosNotas = {'Joao': 10, 'Maria': 14, 'Jose': 8}
+notas = tuple(alunosNotas[x] for x in alunosNotas) # tuple with each grade
+print(notas) #(10, 14, 8)
+
+x = input('Escreva o seu nome: ',)
+if x not in alunosNotas: 
+    print('Erro: esse nome nao consta do registo')
+elif (x in alunosNotas) & (alunosNotas[x] >= 10):
+    print(alunosNotas[x])
+else:
+    print('Rep')
