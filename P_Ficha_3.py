f8994f3d7eeac34e440077e8ece037ea42d20609
Pedro Brito <pedrobfh@gmail.com>
2022-10-11 15:34:55 +0100

    Added exercice 2 from 'Ficha 3'

--- P_Ficha_3.py
+++ P_Ficha_3.py
@@ -14,3 +14,12 @@ elif (x in alunosNotas) & (alunosNotas[x] >= 10):
     print(alunosNotas[x])
 else:
     print('Rep')
+
+# Ex 2
+
+notas = {'Joao': (10,12),'Maria': (9,14),'Jose': (12,8),'Rafael': (6,7)}
+mediaAlunos = {x: (notas[x][0] + notas[x][1])/2 for x in notas}
+print(mediaAlunos) # {'Joao': 11.0, 'Maria': 11.5, 'Jose': 10.0, 'Rafael': 6.5}
+
+notaMaxima = max([notas[x][0] for x in notas]) # gets the maximum value on the array
+print(notaMaxima) # 12
