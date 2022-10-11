2a50ed3b67ad56b79a5e7cb2765da3a1b663335d
Pedro Brito <pedrobfh@gmail.com>
2022-10-11 15:51:59 +0100

    Added exercise 3 from 'Ficha 3'

--- P_Ficha_3.py
+++ P_Ficha_3.py
@@ -23,3 +23,15 @@ print(mediaAlunos) # {'Joao': 11.0, 'Maria': 11.5, 'Jose': 10.0, 'Rafael': 6.5}
 
 notaMaxima = max([notas[x][0] for x in notas]) # gets the maximum value on the array
 print(notaMaxima) # 12
+
+# Ex 3
+
+listaInteiros = [0, 1, 2, 3, 4, 5]
+listaInversa = [listaInteiros[x] for x in range(-1,-len(listaInteiros)-1,-1)] # range starts at -1 and goes to -6 with increments of -1
+print(listaInversa) # [5, 4, 3, 2, 1, 0]
+# or
+listaInversa1 = [listaInteiros[-1-x] for x in range(len(listaInteiros))] # range starts at 0 and goes till 6-1=5
+print(listaInversa1) # [5, 4, 3, 2, 1, 0]
+# or
+listInversa2 = listaInteiros[::-1] # inverts the list
+print(listInversa2) # [5, 4, 3, 2, 1, 0]
