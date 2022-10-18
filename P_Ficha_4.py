534f9e9ad3b34a92be09b980f32de5918a1bfc72
Pedro Brito <pedrobfh@gmail.com>
2022-10-18 14:29:08 +0100

    Added exercise 3 from ficha 4

--- P_Ficha_4.py
+++ P_Ficha_4.py
@@ -17,3 +17,11 @@ def soma(w1, w2):
 a = [2,4,5,6,2,3]
 b = [2,3,4,6,8,9]
 soma(a, b) # [4, 7, 9, 12, 10, 12]
+
+# Ex 3
+r = {'Funchal': [23,22,24,25,21,22,22],
+    'Lisboa': [15,16,18,18,17,19,15],
+    'Porto': [12,15,18,14,10,12,14]}
+def quarta(w):
+    return {x: w[x][2] for x in w}
+quarta(r) # {'Funchal': 24, 'Lisboa': 18, 'Porto': 18}
