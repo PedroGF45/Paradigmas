7d8dd550b47957447f3b459bbb7b73ba85fb8862
Pedro Brito <pedrobfh@gmail.com>
2022-10-18 14:17:44 +0100

    Added exercise 1 from Ficha 4

--- P_Ficha_4.py
+++ P_Ficha_4.py
@@ -1,2 +1,12 @@
 # Defining Functions
 # Ficha 4
+
+# Ex 1
+def checkYear(y):
+    if (((y % 4 == 0) and (y % 100 != 0)) or (y % 400 == 0)):
+        return True
+    else: 
+        return False
+checkYear(1984) # True - E bissexto
+checkYear(2000) # True - E bissexto
+checkYear(1100) # False - Nao e bissexto
