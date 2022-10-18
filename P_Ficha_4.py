4072501527eb4f3c19c1e39eff4a28c25b1b542a
Pedro Brito <pedrobfh@gmail.com>
2022-10-18 14:22:02 +0100

    Added exercise 2 from ficha 4

--- P_Ficha_4.py
+++ P_Ficha_4.py
@@ -10,3 +10,10 @@ def checkYear(y):
 checkYear(1984) # True - E bissexto
 checkYear(2000) # True - E bissexto
 checkYear(1100) # False - Nao e bissexto
+
+# Ex 2
+def soma(w1, w2):
+    return [w1[x] + w2[x] for x in range(len(w1))]
+a = [2,4,5,6,2,3]
+b = [2,3,4,6,8,9]
+soma(a, b) # [4, 7, 9, 12, 10, 12]
