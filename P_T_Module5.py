4d5bc23c1959a52ed6cc828d033d76bfa0619afa
Pedro Brito <pedrobfh@gmail.com>
2022-10-19 11:13:42 +0100

    Added example of factorial function using recursive

--- P_T_Module5.py
+++ P_T_Module5.py
@@ -1,1 +1,12 @@
 # Recursive Programming
+
+# Example of Fatorial function
+def fat(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fat(n - 1)
+fat(5)
+
+def fatOptional(n):
+    return 1 if n == 1 else n * fat(n-1)
