4630e8ad5195dd887d072771eafc2de9ce60cc20
Pedro Brito <pedrobfh@gmail.com>
2022-10-19 11:19:34 +0100

    Added comments to better understand recursive

--- P_T_Module5.py
+++ P_T_Module5.py
@@ -5,9 +5,11 @@ def fat(n):
     if n == 1:
         return 1
     else:
-        return n * fat(n - 1)
+        return n * fat(n - 1) # function will call itself till reach fat(1)
 fat(5) # 120
 
 def fatOptional(n):
-    return 1 if n == 1 else n * fatOptional(n-1)
+    return 1 if n == 1 else n * fatOptional(n-1) # function will call itself till reach fatOptional(1)
 fat(4) # 24
+
+# fat(-1) or fatOptional(-1) would get an infinite loop
