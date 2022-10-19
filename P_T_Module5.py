a01115dba6cd469d290f344d7779ca7c2e82514d
Pedro Brito <pedrobfh@gmail.com>
2022-10-19 11:38:25 +0100

    Added example of recursive exponential function

--- P_T_Module5.py
+++ P_T_Module5.py
@@ -13,3 +13,11 @@ def fatOptional(n):
 fat(4) # 24
 
 # fat(-1) or fatOptional(-1) would get an infinite loop
+
+# Example of exponential function
+def pot(x, n):
+    if n == 0:
+        return 1
+    else:
+        return x * pot(x, n - 1) # recursive
+pot(4, 3) # 64
