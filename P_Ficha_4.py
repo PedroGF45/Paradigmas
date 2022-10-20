71eea1a3bab88c62f05defe429d38806387c06f7
Pedro Brito <pedrobfh@gmail.com>
2022-10-20 15:42:49 +0100

    Added Correction for exercise 4 line c from ficha 4

--- P_Ficha_4.py
+++ P_Ficha_4.py
@@ -60,7 +60,7 @@ h(notas) # {'Joao': 'rep', 'Maria': 'apr', 'Jose': 'rep'}
 a = {'PP': [['Joao',(10,12)],['Maria',(9,14)]],'Calculo': [['Cátia',(8,10)],[ 'Maria',(15,14)],['Vera',(10,7)]]}
 def g(w, d):
     return tuple(w[d][i][1][1] for i in range(len(w[d])))
-g(a, 'Calculo') # (12, 14)
+g(a, 'Calculo') # (10, 14, 7)
 
 #Ex 5
 v1 = ['Joao','Maria','Jose']
