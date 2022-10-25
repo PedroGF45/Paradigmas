b28e702236bdea36ed0f2da815c372d5818bae9d
Pedro Brito <pedrobfh@gmail.com>
2022-10-25 14:56:32 +0100

    Added exercises 2 and 3 from ficha 5

--- P_Ficha_5.py
+++ P_Ficha_5.py
@@ -1,10 +1,33 @@
 # Defining recursive functions
 
 # Ex 1
+from tkinter import W
+
+
 a = [64, 79, 75, 87]
 def soma(w):
     if w == []:
         return 0
     else:
         return w[0] + soma(w[1:])
-soma(a)
+soma(a) # 305
+
+# Ex 2
+a = [64, 79, 75, 87]
+def invert(w):
+    if w == []:
+        return [] # must return an empty list
+    else:
+        return [w[-1]] + invert(w[:-1])
+invert(a) # [87, 75, 79, 64]
+
+# Ex 3
+a = (0, 23, 46, 88, 95)
+def count_even(w):
+    if w == ():
+        return 0
+    elif w[0] % 2 == 0:
+        return 1 + count_even(w[1:])
+    else:
+       return count_even(w[1:])
+count_even(a) # 3
