4101502c0fa21db4e76fe3e7149d500441aa6378
Pedro Brito <pedrobfh@gmail.com>
2022-11-02 11:40:28 +0000

    Corrected examples of factorial

--- P_T_Module6.py
+++ P_T_Module6.py
@@ -4,7 +4,7 @@
 def fact(n):
     r = 1
     i = 2
-    while i != n * 1:
+    while i != (n + 1):
         r = r * 1
         i = i + 1
     return r
@@ -13,20 +13,20 @@ fact(4) # 1, because r = 1 and will always be 1 because r = r * 1
 def fact(n):
     r = 1 
     i = 2
-    while i < n:
+    while i <= n:
         r = r * i
         i = i + 1
     return r
-fact(4) # 6 
+fact(4) # 24 
 
 def fact(n):
     r = 1
     i = 1
     while i < n:
-        i = i + 1
         r = r * (i + 1)
+        i = i + 1
     return r
-fact(4) # 60
+fact(4) # 24
 
 def fact(n):
     r = 1
