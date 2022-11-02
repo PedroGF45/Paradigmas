7be4b5d63c1125168f17e90323da7f8671cdfaea
Pedro Brito <pedrobfh@gmail.com>
2022-11-02 12:18:27 +0000

    Added example using while and for

--- P_T_Module6.py
+++ P_T_Module6.py
@@ -45,3 +45,25 @@ def fact(n):
         i = i - 1
     return r
 fact(4) # 24
+
+# Function that calculates the square of a list of numbers
+# using while
+def squareList(w):
+    r = []
+    j = 0
+    while j < len(w):
+        r = r + [w[j] * w[j]]
+        j = j + 1
+    return r
+a = [1, 2, -3, 5, 10, 4]
+squareList(a) # [1, 4, 9, 25, 100, 16]
+
+# using for
+def squareList(w):
+    r = []
+    j = 0
+    for j in w:
+        r = r + [j * j]
+    return r
+a = [1, 2, -3, 5, 10, 4]
+squareList(a) [1, 4, 9, 25, 100, 16]
