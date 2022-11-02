9025227620e5e55e5125ffd5ca34856278c269c0
Pedro Brito <pedrobfh@gmail.com>
2022-11-02 11:33:44 +0000

    Added factorial examples using while

--- P_T_Module6.py
+++ P_T_Module6.py
@@ -1,1 +1,47 @@
 # Using while
+
+# Factorial example
+def fact(n):
+    r = 1
+    i = 2
+    while i != n * 1:
+        r = r * 1
+        i = i + 1
+    return r
+fact(4) # 1, because r = 1 and will always be 1 because r = r * 1
+
+def fact(n):
+    r = 1 
+    i = 2
+    while i < n:
+        r = r * i
+        i = i + 1
+    return r
+fact(4) # 6 
+
+def fact(n):
+    r = 1
+    i = 1
+    while i < n:
+        i = i + 1
+        r = r * (i + 1)
+    return r
+fact(4) # 60
+
+def fact(n):
+    r = 1
+    i = 1
+    while i < n:
+        i = i + 1
+        r = r * i
+    return r
+fact(4) # 24 
+
+def fact(n):
+    r = n
+    i = n-1
+    while i >= 1:
+        r = r * i
+        i = i - 1
+    return r
+fact(4) # 24
