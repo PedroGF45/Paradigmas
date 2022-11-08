e36e15fc10e97ce632d61615751b153878d1bf0a
Pedro Brito <pedrobfh@gmail.com>
2022-11-08 14:28:18 +0000

    Added exercise 3 from ficha 6

--- P_Ficha_6.py
+++ P_Ficha_6.py
@@ -1,1 +1,13 @@
 # Imperative 
+
+# Ex 3
+def filtra_pares(t):
+    x = 0
+    r = ()
+    while x < len(t):
+        if (t[x] % 2 == 0):
+            r = r + (t[x],)
+        x += 1
+    return r  
+a = (81, 55, 80, 62, 49) # (80, 62)
+filtra_pares(a)
