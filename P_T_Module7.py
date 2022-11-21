ef62cf88ce8120b81b811efeb30410d505716e4a
Pedro Brito <pedrobfh@gmail.com>
2022-11-21 11:16:14 +0000

    Added example for usint filter and anonymous functions

--- P_T_Module7.py
+++ P_T_Module7.py
@@ -1,1 +1,8 @@
-#Conditional Programming (missing classes)ß
+#Conditional Programming (missing classes)
+
+w = [{'cidade': 'Lisboa', 'temp': 25}, {'cidade': 'Porto', 'temp': -5}, {'cidade': 'Funchal', 'temp': 20}, {'cidade': 'Braga', 'temp': -2}]
+
+a = list(filter( lambda x: x['temp'] < 0, w)) # list of cities which temperature is below 0
+def numberNegativeCities(w):
+    return len(w) 
+numberNegativeCities(a) # 2
