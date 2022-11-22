8f2a4ba995975d18c94cc86dbe2484d4b2bd9f8a
Pedro Brito <pedrobfh@gmail.com>
2022-11-22 14:26:15 +0000

    Added exercise 3 from ficha 7

--- P_Ficha_7.py
+++ P_Ficha_7.py
@@ -2,10 +2,15 @@
 
 # Ex 1
 def divs(w, d):
-    return sum((map(lambda x: x, w))) / d
-divs([1, 2, 3, 4, 5], 3) # 5.0
+    return list((map(lambda x: x/d, w))) 
+divs([1, 2, 3, 4, 5], 3) # [0.3333333333333333, 0.6666666666666666, 1.0, 1.3333333333333333, 1.6666666666666667]
 
 # Ex 2
 def primeiros(w):
     return tuple(map(lambda x: x[0], w))
 primeiros([[4, 5, 6, 0],[2, 3, 7, 8],[3, 4, 5, -1]]) # (4, 2, 3)
+
+# Ex 3
+def maiores_iguais_10(w):
+    return list(map(lambda x: x, filter(lambda x: x >= 10, w)))
+maiores_iguais_10([0, 10, -20, 45, 23, 2, 4]) # [10, 45, 23]
