27f96456990c2805ae590beb8ac813f485e28a25
Pedro Brito <pedrobfh@gmail.com>
2022-11-22 14:58:03 +0000

    Added exercise 6 from ficha 7

--- P_Ficha_7.py
+++ P_Ficha_7.py
@@ -14,11 +14,17 @@ primeiros([[4, 5, 6, 0],[2, 3, 7, 8],[3, 4, 5, -1]]) # (4, 2, 3)
 def maiores_iguais_10(w):
     return list(map(lambda x: x, filter(lambda x: x >= 10, w)))
 maiores_iguais_10([0, 10, -20, 45, 23, 2, 4]) # [10, 45, 23]
+#or
+def maiores_iguais_10(w):
+    return list(filter(lambda x: x >= 10, w)) # [10, 45, 23]
 
 # Ex 4
 def soma_listas(w):
     return list(map(lambda x: sum(x), w))
 soma_listas([[4, 5, 6, 0],[2, 3, 7, 8],[3, 4, 5, -1]]) # [15, 20, 11]
+# or
+def soma_listas(w):
+    return list(map(sum, w))
 
 # Ex 5
 a = [['Joao',10],['Maria',17],['Jose',12], ['João',17]]
@@ -29,3 +35,8 @@ media(a) # 14.0
 def minima(w):
     return min(map(lambda x: x[1], w))
 minima(a) # 10
+
+# Ex 6
+def indices_lista(w, e):
+    return list(filter(lambda x: w[x] == e, range(len(w))))
+indices_lista(['a', 2, 'b', 'a'], 'a') # [0, 3]
