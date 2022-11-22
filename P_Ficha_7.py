58f7996b557f4e17255bf14cfce54afcc5024048
Pedro Brito <pedrobfh@gmail.com>
2022-11-22 14:17:26 +0000

    Added exercise 2 from ficha 7

--- P_Ficha_7.py
+++ P_Ficha_7.py
@@ -3,4 +3,9 @@
 # Ex 1
 def divs(w, d):
     return sum((map(lambda x: x, w))) / d
-divs([1, 2, 3, 4, 5], 3)
+divs([1, 2, 3, 4, 5], 3) # 5.0
+
+# Ex 2
+def primeiros(w):
+    return tuple(map(lambda x: x[0], w))
+primeiros([[4, 5, 6, 0],[2, 3, 7, 8],[3, 4, 5, -1]]) # (4, 2, 3)
