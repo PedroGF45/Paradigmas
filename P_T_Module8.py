93ece0e349d34ede5e9c670488f772b1e7437541
Pedro Brito <pedrobfh@gmail.com>
2022-11-23 11:58:26 +0000

    Added algoritmo da selecao direta from module 8

--- P_T_Module8.py
+++ P_T_Module8.py
@@ -10,7 +10,25 @@ def ins_dir(w):
         j = i-1
         while j >= 0 and r[j] <= w[i]:
             j = j - 1
-            r = r[:(j+1)] + (w[i],) + r[(j+1):]
+        r = r[:(j+1)] + (w[i],) + r[(j+1):] # Update the answer
         i = i + 1
     return r
-ins_dir(a)
+ins_dir(a) # (10, 7, 7, 5, 4, 2, 1)
+
+# Algoritmo da Selecao Direta
+a = (9, -6, 4, 5, 7, 2, 10)
+def self_dir(w):
+    r = w
+    n = len(r)
+    i = 0
+    while i<n-1:
+        imin=i
+        j=i+1
+        while j<n:
+            if r[j]<r[imin]:
+                imin=j
+            j=j+1
+        r = r[:i]+(r[imin],)+r[i+1:imin]+(r[i],)+r[imin+1:]
+        i = i + 1
+    return r
+self_dir(a)
