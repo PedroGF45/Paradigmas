69e9b98f4e54a59b1866c53cc26b907bedbf5060
Pedro Brito <pedrobfh@gmail.com>
2022-12-03 09:59:33 +0000

    Added same examples with for

--- Revisoes.py
+++ Revisoes.py
@@ -3,7 +3,8 @@
 p = [('Ana', 14), ('Ze', 18), ('Joao', 9), ('Martim', 12)]
 q = [{'nome': 'Ana', 'nota': 14}, {'nome': 'Ze', 'nota': 18}, {'nome': 'Joao', 'nota': 9}, {'nome': 'Martim', 'nota': 12}]
 
-# Counting problems with a list of tuples and a list of dictionaries
+# Counting problems with a list of tuples and a list of dictionaries using while and for
+# using while with list of tuples
 def f(w, x):
     i = 0
     r = 0
@@ -14,6 +15,16 @@ def f(w, x):
     return r
 f(p, 10) # 3 
 
+# using for with list of tuples
+def f_for(w, x):
+    r = 0
+    for i in range(len(w)):
+        if (w[i][1] > x):
+            r += 1
+    return r
+f_for(p,10) # 3
+
+# using while with list of dictionaries
 def f_o(w, x):
     i = 0 
     r = 0
@@ -23,3 +34,12 @@ def f_o(w, x):
         i += 1
     return r
 f_o(q, 10) # 3
+
+# using for with list of dictionaries
+def f_o_for(w, x):
+    r = 0
+    for i in range(len(w)):
+        if (w[i]['nota'] > x):
+            r += 1
+    return r
+f_o_for(q, 10) # 3
