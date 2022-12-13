b40cc5a19ff19a61022364de16f9175093e55570
Pedro Brito <pedrobfh@gmail.com>
2022-12-13 15:23:56 +0000

    Added exercise 1 from ficha 9

---
+++ P_Ficha_9.py
@@ -0,0 +1,39 @@
+# Exercise 1
+class sequencia:
+    def __init__(self):
+        self._ri = [] # _ri -> representacao interna
+
+    def acr(self, n):
+        self._ri.insert(0, n)
+
+    def vaziaQ(self):
+        return self._ri == []
+
+    def primeiro(self):
+        if self.vaziaQ():
+            print('sequencia vazia')
+        else:
+            return self._ri[0]
+
+    def tiraPrim(self):
+        if self.vaziaQ():
+            print('sequencia vazia')
+        else:
+            del(self._ri[0])
+
+    def comp(self):
+        return len(self._ri)
+
+    def retPares(self):
+        i = self.comp() - 1
+        while i >= 0:
+            if self._ri[i] % 2 == 0:
+                del(self._ri[i])
+            i += -1
+
+    def insereOrdenada(self, n):
+        i = 0
+        k = self.comp()
+        while i < k and self._ri[i] <= n:
+            i += 1
+        self._ri.insert(i, n)
