9056b458fa9dcad8adc8f9befed92e800f5ed0c8
Pedro Brito <pedrobfh@gmail.com>
2022-12-28 18:00:00 +0000

    Added opeartions for the waiting list of airplanes

---
+++ Projeto_Aeroporto/filas.py
@@ -0,0 +1,25 @@
+class fila:
+    
+    def __init__(self):
+        self._ri = []
+        
+    def entra(self,elemento):
+        self._ri.insert(len(self._ri),elemento)
+        
+    def vaziaQ(self):
+        return self._ri == []
+    
+    def sai(self):
+        if self.vaziaQ():
+            print('operação sai aplicada a fila vazia')
+        else:
+            del(self._ri[0])
+            
+    def primeiro(self):   
+        if self.vaziaQ():
+            print('operação PRIMEIRO aplicada a fila vazia')
+        else:
+            return self._ri[0]
+        
+    def comp(self):
+        return len(self._ri)
