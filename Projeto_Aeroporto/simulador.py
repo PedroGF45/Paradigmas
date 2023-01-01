685cd6fe83d64c7432a42b1bd9d32267aa1c44b8
Pedro Brito <pedrobfh@gmail.com>
2023-01-01 22:42:09 +0000

    Added comments to better understand the code

--- Projeto_Aeroporto/simulador.py
+++ Projeto_Aeroporto/simulador.py
@@ -2,7 +2,7 @@ from random import random, randint
 from math import log
 from eventos import evento
 from cadeias import cap
-from filas import fila_fategem, fila_fdesgem
+from filas import fila_aterragem, fila_descolagem
 
 def simula(mc1, mc2, ma, me, md, x, ts):
     
@@ -16,8 +16,8 @@ def simula(mc1, mc2, ma, me, md, x, ts):
         global c, fater, fdesc, ic, pe, pista, tmed, tmeap, tmeanp, nmea, nad, nma, p
     
         c = cap()
-        fater = fila_fategem()
-        fdesc = fila_fdesgem()
+        fater = fila_aterragem()
+        fdesc = fila_descolagem()
         ic = 0
 
         if 1/x == x:
@@ -44,17 +44,16 @@ def simula(mc1, mc2, ma, me, md, x, ts):
             p = 'nao_prioritario'
 
         if evt.cat() == 'chega':
-            if (ts > 180 and ts < 660):
+            if (ts > 180 and ts < 660): # arrival of new planes depending on the current time of the simulation
                     e = evento(ic + obsexp(mc2), 'chega', 'indiferente') 
                     c.acr(e)
-                    
             else:
                     e = evento(ic + obsexp(mc1), 'chega', 'indiferente')
                     c.acr(e)
             
-            if pista == 'livre' and evt.cat() == 'chega':
-                if fater.vaziaQ:
-                    e = evento(ic + obsexp(me), 'fate', 'indiferente')
+            if pista == 'livre' and evt.cat() == 'chega': 
+                if fater.vaziaQ(): # check if queue is empty
+                    e = evento(ic + obsexp(me), 'fate', 'indiferente') # add next event which is end of landing
                     c.acr(e)
                     pista = 'ocupada'
                 else:
@@ -67,23 +66,10 @@ def simula(mc1, mc2, ma, me, md, x, ts):
                     if fater.comp() > nmea:
                         nmea = fater.comp()
 
-            if pista == 'livre' and evt.cat() == 'fdes':
-                if fdesc.vaziaQ:
-                    pista = 'ocupada'
-                else:
-                    e = evento(ic + obsexp(me), 'fdes', 'indiferente')
-                    c.acr(e)
-                    fdesc.sai()
-                    pista = 'ocupada'
-            elif pista == 'ocupada' and evt.cat() == 'fdes': 
-                    e = evento(ic + obsexp(me), 'fdes', 'indiferente')
-                    c.acr(e)
-                    fdesc.entra(ic) 
                     
-
         elif evt.cat() == 'fate' or evt.cat() == 'fdes':
 
-            # checkar se existe algum aviao prioritario e manda-lo fater
+            # checkar se existe algum aviao prioritario e manda-lo aterrar
             # se existerem menos de y avioes para fdesr, podem fater os outros avioes
             # se nao existerem avioes nas filas, pista livre         
                 
@@ -95,13 +81,19 @@ def simula(mc1, mc2, ma, me, md, x, ts):
             e = evento(ic + obsexp(md), 'fdes', 'indiferente')
             c.acr(e)
 
-        else: # evt.cat() == 'fdes' 
-            if f.vaziaQ():
-                pista = 'livre'
-            else: 
-                fdesc.entra(ic)
-                e = evento(ic + obsexp(md), 'fdes', 'indiferente')
-                c.acr(e)
+        else:
+            if pista == 'livre' and evt.cat() == 'fdes':
+                    if fdesc.vaziaQ(): # check if queue is empty
+                        pista = 'ocupada'
+                    else:
+                        e = evento(ic + obsexp(me), 'fdes', 'indiferente') # add next event which is end of take-off
+                        c.acr(e)
+                        fdesc.sai()
+                        pista = 'ocupada'
+            elif pista == 'ocupada' and evt.cat() == 'fdes': 
+                    e = evento(ic + obsexp(me), 'fdes', 'indiferente')
+                    c.acr(e)
+                    fdesc.entra(ic) 
 
 
 
